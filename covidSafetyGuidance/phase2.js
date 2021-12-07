098be5880b994a4744295bdfbd2cd651dc43b208
Mariela <90412861+maryelogs@users.noreply.github.com>
2021-12-06 22:38:20 -0500

    Add files via upload

---
+++ covidSafetyGuidance/phase2.js
@@ -0,0 +1,33 @@
+// sticky header: the function will be executed when a user scrolls the page
+
+window.onscroll = function() {myFunction()};
+
+var header = document.getElementById("myHeader");
+
+var sticky = header.offsetTop;
+
+function myFunction() {
+    if (window.pageYOffset > sticky) {
+        header.classList.add("sticky");
+    } else {
+        header.classList.remove("sticky");
+    }
+}
+
+
+// javascript for the collapsible section of the web page
+
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
