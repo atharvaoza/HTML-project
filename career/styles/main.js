d1b1d986a0c935c304b87b626553f0b7638e7b66
Nimratpreet <90514317+Nimratpreet@users.noreply.github.com>
2021-12-13 16:33:29 -0500

    Add files via upload

---
+++ career/styles/main.js
@@ -0,0 +1,9 @@
+function showPositions(){
+    var x = document.getElementById("show");
+    if (x.style.display == "none"){
+        x.style.display = "block";
+    } else {
+        x.style.display ="none";
+    }
+       
+    }
