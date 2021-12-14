6e81d022dcc86e4a1339cdf014f5467ea5c237e3
atharvaoza <43638761+atharvaoza@users.noreply.github.com>
2021-12-14 01:16:52 -0500

    Add files via upload

--- EventHosting/page3/EventHostJS.js
+++ EventHosting/page3/EventHostJS.js
@@ -1,3 +1,7 @@
+'use strict';
+
+const $ = (selector) => document.querySelector(selector);
+
 let today = new Date();
 
 let day = today.getDate();
@@ -16,10 +20,61 @@ today = day + "/" + month + "/" + year;
 // document.getElementById("datePicker1").setAttribute("min", today);
 document.getElementById("datePicker1").min = today;
 
-function validateForm()
-{
-    if(document.getElementById("nameEvent").value.length == "0")
-    {
-        alert("empty field")
+
+// function validateForm()
+// {
+//     if(document.getElementById("nameEvent").value.length == "0")
+//     {
+//         alert("empty field")
+//     }
+// }
+
+
+// function validateForm()
+// {
+//     let x = document.getElementById("eventName").value;
+//     let text;
+//     if(x == "")
+//     {
+//         text = "Text field empty"
+//     }
+//     document.getElementById("demo").innerHTML = text;
+// }
+
+const processEntries = () => {
+    //get form controls to check for validity
+    const name = $('#nameEvent');
+    const ven = $('#venue');
+    const description = $('#desc');
+
+    let isValid = true;
+    if (name.value == '') {
+        name.nextElementSibling.textContent = 'This is a required field.';
+        isValid = false;
+    } else {
+        name.nextElementSibling.textContent = '';
     }
+
+    if (ven.value == '') {
+        ven.nextElementSibling.textContent = 'This is a required field.';
+        isValid = false;
+    } else {
+        ven.nextElementSibling.textContent = '';
+    }
+
+    if (description.value == '') {
+        description.nextElementSibling.textContent = 'This is a required field.';
+        isValid = false;
+    } else {
+        description.nextElementSibling.textContent = '';
+    }
+//submit form if all fields are valid
+if (isValid == true) {
+    $('form').submit();
 }
+};
+
+document.addEventListener('DOMContentLoaded', () => {
+    $('#subButton').addEventListener('click', processEntries);
+
+    });
