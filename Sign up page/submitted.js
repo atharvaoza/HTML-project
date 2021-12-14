723ec9c570122f1a6ec72d78e55d7076db05a604
Mariela <90412861+maryelogs@users.noreply.github.com>
2021-12-13 21:34:47 -0500

    Delete submitted.js

--- Sign up page/submitted.js
+++
@@ -1,5 +0,0 @@
-const submittedList = document.getElementById('submitted')
-           new URLSearchParams(window.location.search).forEach(value,name) => {
-               submittedList.append('S{name}: ${value}')
-               submittedList.append(document.createElement('br'))
-           }
